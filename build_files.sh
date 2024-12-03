236ffef2810d4d383459dcfe705f9707aa30a051
MartinVelez2002 <martinvelezjr@hotmail.com>
2024-12-02 22:20:16 -0500

    Modificacion

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
 # build_files.sh
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
-
-echo "Aplicando migraciones..."
-python3.9 manage.py makemigrations || { echo "Error al ejecutar makemigrations"; exit 1; }
-python3.9 manage.py migrate || { echo "Error al ejecutar migrate"; exit 1; }
-
-echo "Recolectando archivos estáticos..."
-python3.9 manage.py collectstatic --noinput || { echo "Error al recolectar archivos estáticos"; exit 1; }
-
-echo "Script ejecutado con éxito."
